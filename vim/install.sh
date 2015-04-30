961523ee7b858e4842db448580cfdcbdbc4ef7f5
flickdm <flickdm@mail.uc.edu>
2015-04-29 21:41:21 -0400

    added to the vim/install.sh

--- vim/install.sh
+++ vim/install.sh
@@ -14,9 +14,11 @@ git clone https://github.com/duythinht/inori
 git clone https://github.com/bling/vim-airline.git
 git clone git://github.com/tpope/vim-fugitive.git && vim -u NONE -c "helptags vim-fugitive/doc" -c q
 git clone https://github.com/jelera/vim-javascript-syntax.git ~/.vim/bundle/vim-javascript-syntax
+git clone https://github.com/gmarik/Vundle.vim.git
 
 # ---------------------------- COLORS ----------------------------------------------------
 
+cd ~/.vim/ && mkdir colors/
 cd ~/.vim/colors/ && \
 curl https://raw.githubusercontent.com/duythinht/inori/master/colors/inori.vim >> inori.vim
 chmod 777 inori.vim
